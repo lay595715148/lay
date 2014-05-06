3b66339dafc751ddedea9dab52c2f2b045fb0d10
lay595715148 <lay595715148@126.com>
2014-05-06 15:55:44 +0800

    add sso test plugin

---
+++ plu/sso/index.php
@@ -0,0 +1,21 @@
+<?php
+class SSO extends Plugin {
+    public function initilize() {
+        $this->addHook(Lay::HOOK_INIT, array($this, 'sayhello'));
+        $this->addHook(Action::HOOK_CREATE, array($this, 'sayhello2'));
+        $this->addHook(Action::HOOK_DESTROY, array($this, 'sayhello3'));
+    }
+    public function sayhello() {
+        //Lay::s
+        //Web::test();echo '<br>';
+        Logger::debug('SSO say hello!');
+    }
+    public function sayhello2($action) {
+        Logger::debug('SSO say hello2!');
+    }
+    public function sayhello3($action) {
+        //Web::test();echo '<br>';
+        Logger::debug('SSO say hello3!');
+    }
+}
+?>
