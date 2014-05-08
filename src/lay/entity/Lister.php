0911c911bd5112f987603b8f001368ed7712ab49
lay595715148 <lay595715148@126.com>
2014-05-08 17:52:25 +0800

    add Lister

---
+++ src/lay/entity/Lister.php
@@ -0,0 +1,29 @@
+<?php
+if(! defined('INIT_LAY')) {
+    exit();
+}
+
+class Lister extends Entity {
+    public function __construct() {
+        parent::__construct(array(
+            'list' => array(),
+            'total' => 0,
+            'hasNext' => false
+        ), array(
+            'list' => Bean::PROPETYPE_ARRAY,
+            'total' => Bean::PROPETYPE_INTEGER,
+            'hasNext' => Bean::PROPETYPE_BOOLEAN
+        ));
+    }
+    public function summary() {
+        return array(
+            'list' => 'list',
+            'total' => 'total',
+            'hasNext' => 'hasNext'
+        );
+    }
+    public function toSummary() {
+        return $this->toArray();
+    }
+}
+?>
