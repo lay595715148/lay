9aa4ef1c431a9ba9524211665831fba4a7ca4e67
lay595715148 <lay595715148@126.com>
2014-05-23 10:25:13 +0800

    move plugin dir./

--- src/lay/core/I_EventEmitter.php
+++
@@ -1,9 +0,0 @@
-<?php
-if(! defined('INIT_LAY'))
-    exit();
-
-interface I_EventEmitter {
-    public function trigger($eventid, array $params = array());
-    public function register($eventid, $func, $level = 0);
-}
-?>
