1d11eb618785c894ffe61d8959addc780b826c88
lay595715148 <lay595715148@126.com>
2014-06-06 17:54:01 +0800

    add ExpireIncrementer

---
+++ src/lay/model/ExpireIncrementer.php
@@ -0,0 +1,15 @@
+<?php
+namespace lay\model;
+
+use lay\core\Model;
+use lay\core\I_Expireable;
+use lay\core\I_Increment;
+
+/**
+ * 可设置自增涨键和过期时间的模型对象
+ * @author Lay Li
+ */
+abstract class ExpireIncrementer extends Model implements I_Expireable, I_Increment {
+    
+}
+?>
